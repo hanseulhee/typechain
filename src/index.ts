bf61e131cc5fe250dfdb22a3c2a5f56fbf320811
hanseulhee <3021062@gmail.com>
2022-01-21 16:01:38 +0900

    Feat: Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import * as CryptoJS from "crypto-js";
+import { isBlock } from "typescript";
 
 class Block{
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string
+        ):string => 
+        CryptoJS.SHA256(index + previousHash + timestamp +data).toString();
+
+    static validateStructure = (aBlock: Block) : boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" && 
+    typeof aBlock.timestamp === "number" && 
+    typeof aBlock.data === "string";
+
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
     
-    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string):string => CryptoJS.SHA256(index + previousHash + timestamp +data).toString();
-
-
     constructor(
         index: number,
         hash: string,
@@ -30,22 +43,51 @@ class Block{
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 let blockchain: [Block] = [genesisBlock]
 
-const getBlockchain = () : Block[] => blockchain;// Block 배열을 리턴함
+const getBlockchain = () : Block[] => blockchain;// Block 배열을 리턴한다.
 
-const getLatesBlock = () : Block => blockchain[blockchain.length -1]
+const getLatestBlock = () : Block => blockchain[blockchain.length -1]
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
 
 const createNewBlock = (data:string): Block => {
-    const previousBlock: Block = getLatesBlock();
+    const previousBlock: Block = getLatestBlock();
     const newIndex : number =previousBlock.index +1;
     const newTimestamp: number = getNewTimeStamp();
     const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
-    const newBlock : Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+
+    const newBlock : Block = new Block(
+        newIndex, 
+        newHash, 
+        previousBlock.hash, 
+        data, 
+        newTimestamp
+    );
     return newBlock;
 
 }
 
-console.log(createNewBlock("Hello"), createNewBlock("Bye Bye"))
+const getHashforBlock = (aBlock: Block) : string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data)
+
+const isBlockVaild = (candidateBlock : Block, previousBlock: Block) : boolean => {
+    // 블록의 구조가 유효한가
+    // candidateBlock, previousBlock을 받고 유효하지 않으면 False을 리턴한다.
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    }else if(previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    }else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    }else{
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block) : void => {
+    if(isBlockVaild(candidateBlock, getLatestBlock())){
+        blockchain.push(candidateBlock);
+    }
+}
 
 export {}
